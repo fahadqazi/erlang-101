dac27a5bcdfd869ada2cd51c4e1bc47c2a29b0d0
Fahad Qazi <fahadqazi@gmail.com>
2017-06-20 00:54:59 +0100

    tripple and area functions

--- first.erl
+++ first.erl
@@ -1,8 +1,15 @@
 -module(first).
--export([double/1]).
+-export([double/1, mult/2, tripple/1, area/3]).
 
 mult(X, Y) ->
     X * Y.
 
 double(X) ->
     mult(2, X).
+
+tripple(X) ->
+    mult(3, X).
+
+area(A, B, C) ->
+    S = (A + B + C)/2,
+    math:sqrt(S*(S-A)*(S-B)*(S-C)).
