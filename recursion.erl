1ac7d797cb7206af391b9d3ee162b965dc6b7146
Fahad Qazi <fahadqazi@gmail.com>
2017-06-20 22:36:15 +0100

    fibonacci

--- recursion.erl
+++ recursion.erl
@@ -1,5 +1,9 @@
 -module(recursion).
--export([factorial/1]).
+-export([factorial/1, fib/1]).
 
 factorial(0) -> 1;
-factorial(X) when N>0 -> factorial(X-1)*X.  %when N>0 is to avoid negative infinite recursion
+factorial(X) when X>0 -> factorial(X-1)*X.  %when N>0 is to avoid negative infinit 
+
+fib(0) -> 1;
+fib(1) -> 1;
+fib(N) -> fib(N-1) + fib(N-2).
