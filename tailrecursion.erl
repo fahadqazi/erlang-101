5d19d932a8d8b782aa95a8e075c3904586602b43
Fahad Qazi <fahadqazi@gmail.com>
2017-06-20 23:39:34 +0100

    adding a loop construct

--- tailrecursion.erl
+++ tailrecursion.erl
@@ -1,2 +1,16 @@
 -module(tailrecursion).
--export([]).
+-export([fac/1, fact/2, loop/1]).
+
+fac(N) ->
+    fact(N,1).
+
+fact(0,P) ->
+    P;
+fact(N,P) when N>0 ->
+    fact(N-1, N*P).
+
+loop(N) when N>0 ->
+    io:format("~p~n", [N]),
+    loop(N-1);
+loop(N) ->
+    io:format("bye~n").
